e5eaf62120037f6b6377247159404be304a92ee6
Nicolas Sousa <niolaspaixao34@gmail.com>
2026-09-10 01:15:26 -0300

    feat: cria componentes Header e Footer com Tailwind e atualiza rotas

--- my-app/src/router/AppRoutes.tsx
+++ my-app/src/router/AppRoutes.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import Home from '../pages/Home';
 import Sobre from '../pages/Sobre';
 import Faq from '../pages/Faq';
@@ -10,15 +12,25 @@ import Dashboard from '../pages/Dashboard';
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/contato" element={<Contato />} />
-        <Route path="/integrantes" element={<Integrantes />} />
-        <Route path="/simulador" element={<Simulador />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      {/* Estrutura Global simulando o antigo global.css no body */}
+      <div className="bg-[#050816] text-[#ffffff] min-h-screen flex flex-col font-sans overflow-x-hidden">
+        <Header />
+        
+        {/* Container principal simulando a antiga tag main */}
+        <main className="mt-[100px] p-[40px_5%] max-w-[1400px] mx-auto w-full flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path="/integrantes" element={<Integrantes />} />
+            <Route path="/simulador" element={<Simulador />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
