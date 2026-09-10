ba04e119000e5b33726762f4cd12a05db8556fd7
Nicolas Sousa <niolaspaixao34@gmail.com>
2026-09-10 02:19:40 -0300

    feat: atualiza e simplifica tsx

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -4,7 +4,6 @@ import FeatureCard from '../components/FeatureCard';
 export default function Home() {
   return (
     <>
-      {/* Hero Section */}
       <section className="flex flex-col md:flex-row items-center justify-between gap-[40px] bg-[#10172a] p-[40px_20px] md:p-[60px_40px] rounded-[16px] shadow-[0_20px_45px_rgba(0,0,0,0.35)] mb-[50px] border border-[#00f5c8]/20 text-center md:text-left">
         <div className="flex-1">
           <h2 className="text-[2.2rem] md:text-[3rem] text-white mb-[20px] leading-[1.2] font-bold">
@@ -24,7 +23,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Features Section */}
       <section className="grid grid-cols-1 md:grid-cols-3 gap-[30px] mb-[50px]">
         <FeatureCard 
           title="1. Ação no Dia a Dia" 
--- my-app/src/pages/Integrantes.tsx
+++ my-app/src/pages/Integrantes.tsx
@@ -4,8 +4,8 @@ export default function Integrantes() {
   const membros = [
     { nome: "Maria Eduarda Escandor", rm: "568216", turma: "1TDSPO", foto: "/img/foto-maria1.jpeg", linkedin: "https://www.linkedin.com/in/maria-eduarda-escandor-5b1587359/", github: "https://github.com/mariabatistaescandor-gif" },
     { nome: "Erick Menezes", rm: "570325", turma: "1TDSPF", foto: "/img/foto-erick.jpeg", linkedin: "https://www.linkedin.com/in/erick-menezes-b53009232/", github: "https://github.com/Erick488-maker" },
-    { nome: "Katerine Hildebrand", rm: "569809", turma: "1TDSPF", foto: "/img/foto-katerine.jpeg", linkedin: "https://www.linkedin.com/in/katerine-hildebrand-8928752a3/", github: "https://github.com/katpeaga" },
     { nome: "Maria Eduarda Lopes de Lima", rm: "572425", turma: "1TDSPO", foto: "/img/foto-maria2.png", linkedin: "https://www.linkedin.com/in/maria-eduarda-lopes-de-lima-1291b6289/", github: "https://github.com/mariaeduardaalima" },
+    { nome: "Matheus Yudi ", rm: " 571245", turma: "1TDSPO", foto: "/img/foto-maria2.png", linkedin: "https://www.linkedin.com/in/maria-eduarda-lopes-de-lima-1291b6289/", github: "https://github.com/mariaeduardaalima" },
     { nome: "Nicolas Sousa", rm: "574141", turma: "1TDSPH", foto: "/img/foto-nicolas.jpg", linkedin: "https://www.linkedin.com/in/nicolas-sousaa/", github: "https://github.com/nicolaspk" }
   ];
 
